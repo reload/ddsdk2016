b874b5014666e275061a301ac54e7bcc31ba4e08
Troels Lenda <troelslenda@gmail.com>
2016-09-13 12:11:35 +0200

    Extending gulp config with: -javascript assets copying and linting. -include Sass Bourbon. -cleaning of removed files. DDSDK-41

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,51 @@
  */
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var sass = require('gulp-sass');
+var jshint  = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var sourcemaps = require('gulp-sourcemaps');
 var notify = require('gulp-notify');
+var bourbon =  require('node-bourbon');
 
 
 var path = {
-  sass: 'theme_assets/mungo/sass/',
-  css: 'web/themes/custom/mungo/css'
+  sass: 'theme_assets/mungo/sass',
+  css: 'web/themes/custom/mungo/css',
+  js_src: 'theme_assets/mungo/javascript',
+  js_dest: 'web/themes/custom/mungo/js'
 };
 
 gulp.task('sass', function () {
   return gulp.src(path.sass + '/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', notify.onError(function (error) {
+    .pipe(sass({
+      includePaths: bourbon.includePaths,
+      outputStyle: 'expanded'
+    }).on('error', notify.onError(function (error) {
       return "\n\nSASS error: " + error.message;
     })))
     .pipe(sourcemaps.write('.'))
-
     .pipe(gulp.dest(path.css))
+});
+
+gulp.task('javascript', function () {
+  return gulp.src(path.js_src + '/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+
+    .pipe(gulp.dest(path.js_dest))
 });
 
 gulp.task('watch', function () {
   gulp.watch([path.sass + '/*.scss', path.sass + '/*/*.scss'], ['sass']);
+  gulp.watch(path.js_src + '/*.js', ['javascript']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('clean', function () {
+  return gulp.src([path.css, path.js_dest], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'sass', 'javascript', 'watch']);
