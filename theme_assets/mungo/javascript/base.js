1997efb9029b47370aaaa45d987a9e8e5db9df66
Troels Lenda <troelslenda@gmail.com>
2016-09-19 10:57:51 +0200

    Refactor header-fixed positioning. DDSDK-48

--- theme_assets/mungo/javascript/base.js
+++ theme_assets/mungo/javascript/base.js
@@ -1,8 +1,14 @@
-jQuery(function($){
-  /**
-   * Position the site menu when toolbar is active.
-   */
-  $(document).on('drupalViewportOffsetChange.toolbar', function (event, offsets) {
-    $('header.header-fixed').offset(offsets);
-  });
-});
+(function ($, Drupal) {
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context, settings) {
+      /**
+       * Position the site menu when toolbar is active.
+       */
+      $(document).on('drupalViewportOffsetChange.toolbar', function (event, offsets) {
+        console.log(offsets.top);
+        $('header.header-fixed').css('top',(offsets.top));
+        $('header.header-fixed').css('left',(offsets.left));
+      });
+    }
+  };
+})(jQuery, Drupal);
