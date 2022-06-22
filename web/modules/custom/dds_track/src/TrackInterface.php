b698910d82972d7854a250263287c0564e272bd8
Mikkel Blyme <mb@reload.dk>
2022-06-22 10:29:43 +0200

    Move global variables to interface

---
+++ web/modules/custom/dds_track/src/TrackInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\dds_track;
+
+/**
+ * Provides an interface for dds_track.
+ */
+interface TrackInterface {
+
+  /**
+   * The path to the frontpage of the track-universe.
+   */
+  const TRACK_UNIVERSE_FRONTPAGE = '/track-universet';
+
+  /**
+   * The ID of the parent track term.
+   *
+   * Since the name of the "Track" term can change, this is a helper function
+   * that returns the id of the term associated with the "Track" term.
+   */
+  const TRACK_TERM_ID = 834;
+
+}
